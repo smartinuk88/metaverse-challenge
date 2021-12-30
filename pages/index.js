f51a6fe2e28488e10d1e771ce78a37fe0e238d8e
Scott Martin <scott.martin88@live.com>
2021-12-30 10:50:09 +0100

    chat functionality complete

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,24 @@
 import Head from "next/head";
 import Login from "../components/Login";
 import { useMoralis } from "react-moralis";
+import Header from "../components/Header";
+import Messages from "../components/Messages";
 
 export default function Home() {
   const { isAuthenticated } = useMoralis();
 
   if (!isAuthenticated) return <Login />;
   return (
-    <div className="h-screen">
+    <div className="h-screen overflow-y-scroll bg-gradient-to-b from-black to-fuchsia-900 overflow-hidden">
       <Head>
         <title>Metaverse Challenge</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Welcome to the APP</h1>
+      <div className="max-w-screen-2xl mx-auto">
+        <Header />
+        <Messages />
+      </div>
     </div>
   );
 }
